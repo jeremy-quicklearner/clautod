82067597999a143a87e5b58dc245a6fab9794d3a
Jeremy Lerner <jeremy.quicklearner@gmail.com>
2018-06-12 20:50:49 -0700

    Parametrized version in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
 import setuptools
+import re
 
+# Pull description from README
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# Pull version from version file, populated before
+with open("clautod-version.txt", "r") as fh:
+    lines = fh.readlines()
+
+# Filter out comments and whitespace
+lines = [line.strip() for line in lines]
+lines = [line for line in lines if not line == ""]
+lines = [line for line in lines if not line.startswith('#')]
+
+# Sanity checks
+if not len(lines) == 1:
+    print("clautod-version contents invalid")
+    exit(1)
+if not re.match(r"^[0-9]+\.[0-9]+\.[0-9]+\-[0-9][0-9][0-9][0-9]$", lines[0]):
+    print("Invalid version <" + lines[0] + ">")
+    exit(1)
+clautod_version = lines[0]
+
 setuptools.setup(
     name="clautod",
-    version="0.0.1-0001",
+    version=clautod_version,
     author="Jeremy Lerner",
     author_email="jeremy.cpsc.questions@gmail.com",
     description="Consumables-Listing AUTOmation Daemon",
