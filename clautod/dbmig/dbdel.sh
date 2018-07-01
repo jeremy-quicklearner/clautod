f1677821c96c2c16657116034139cde680460ccb
Jeremy Lerner <jeremy.quicklearner@gmail.com>
2018-07-01 09:59:27 -0700

    Added dbmig directory

---
+++ clautod/dbmig/dbdel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script deeltes the Clauto database.
+# It should be run from the clautod postrm script on
+# uninstallation of clautod.
+
+echo "[dbdel] HELLO I AM DBDEL"
---
+++ clautod/dbmig/dbmig.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script ensures the Clauto database is up-to-date.
+# It should be run from the clautod postinst script on
+# installation of clautod.
+
+echo "[dbmig] HELLO I AM DBMIG"
+
+# If the database directory doesn't exist, create it
+#if [ ! -d /etc/clauto/clautod/db ]; then
+#    echo "[dbmig] Database directory not found. Creating."
+#    mkdir -p /etc/clauto/clautod/db
+#fi
+
+# If the database doesn't exist, create it
