39d8671a6078a5f21b15f26a3394d44aac4e5ce8
Jeremy Lerner <jeremy.quicklearner@gmail.com>
2018-07-01 14:06:03 -0700

    devscripts are now in their own folder

--- clautod/dbmig/dbmig.sh
+++ clautod/dbmig/dbmig.sh
@@ -5,16 +5,50 @@
 # installation of clautod.
 
 DB_FILE=/etc/clauto/clautod/clauto.db
+DB_SCRIPTS_DIR=/usr/share/clauto/clautod/dbmig
 
 # If the database doesn't exist, create it with USER_VERSION 0
 if [ ! -f $DB_FILE ] ; then
     echo "[dbmig] Clauto database not found. Creating empty database."
     touch $DB_FILE
-    sqlite3 $DB_FILE < 0.sql
+    sqlite3 $DB_FILE < $DB_SCRIPTS_DIR/0.sql
 fi
 
 # Determine what version the existing database is at
 DB_VERSION_CURRENT=$(sqlite3 $DB_FILE 'PRAGMA user_version')
 
 # Determine what version the database should be
-DB_VERSION_GOAL=$(cat dbversion.txt)
+DB_VERSION_GOAL=$(cat $DB_SCRIPTS_DIR/dbversion.txt)
+
+
+# Apply each script in order until the goal version is reached
+echo -n "[dbmig] Migrating Clauto database to version(s) "
+while [ $DB_VERSION_CURRENT -ne $DB_VERSION_GOAL ] ; do
+    # Determine the next database version
+	let DB_VERSION_NEXT=$DB_VERSION_CURRENT+1
+
+    # Ensure a migration script exists for that version
+    if [ ! -f $DB_SCRIPTS_DIR/$DB_VERSION_NEXT.sql ] ; then
+        echo ; >&2 echo "[dbmig] Error: Migration script for database version <" $DB_VERSION_NEXT "> not found. Unable to migrate to version <" $DB_VERSION_GOAL ">"
+        >&2 echo "Clauto Database migration failed"
+        exit 1
+    fi
+
+    # Apply the migration script
+    echo -n $DB_VERSION_NEXT"..."
+    sqlite3 $DB_FILE < $DB_SCRIPTS_DIR/$DB_VERSION_NEXT.sql
+
+    # Ensure the migration script updated the user_version pragma
+    if [ $(sqlite3 $DB_FILE 'PRAGMA user_version') -ne $DB_VERSION_NEXT ] ; then
+        echo ; >&2 echo "[dbmig] Error: Migration script for database version <" $DB_VERSION_NEXT "> didn't update user_version pragma. Unable to migrate to version <" $DB_VERSION_GOAL ">"
+         >&2 echo "Clauto Database migration failed"
+        exit 1
+    fi
+
+    # Update the current version
+    DB_VERSION_CURRENT=$(sqlite3 $DB_FILE 'PRAGMA user_version')
+done
+
+# Migration is complete
+echo ; echo "[dbmig] Clauto database is up-to-date at version <" $DB_VERSION_CURRENT ">"
+exit 0
---
+++ devscripts/release.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Script for releasing clautod
+# Given a version number, this script creates a debian package
+# from the clautod source, adds it to the local repo, and tags
+# the current git commit with the same version number. This is
+# the clautod release flow.
+# The script is almost certainly broken on all machines except
+# my home PC where it's intended to be run. Don't try it!
+
+# Check parameters
+if [ ! $1 ] ; then
+    echo "[release] Specify version string in X.X.X format"
+    exit 1
+fi
+if [[ ! $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] ; then
+    echo "[release] Version string <"$1"> is invalid. Use X.X.X format"
+    exit 1
+fi
+
+echo "[release] Releasing clautod v"$1"..."
+echo "[release] Preparing package tree..."
+
+# Populate README files in debian directory
+cp ../README.md ../debian/README.debian
+cp ../README.md ../debian/README.source
+
+# Populate changelog based on git log
+echo "clautod ("$1") unstable; urgency=medium"                           > ../debian/changelog
+echo ""                                                                 >> ../debian/changelog
+git log --oneline $(git tag | sort -V | tail -n 1)..@ | sed 's/^/  * /' >> ../debian/changelog
+echo ""                                                                 >> ../debian/changelog
+echo " -- Jeremy Lerner <jeremy.cpsc.questions@gmail.com>  "$(date -R)  >> ../debian/changelog
+
+# Let the user edit the changelist
+nano ../debian/changelog
+
+echo "[release] Package tree ready. Building package..."
+
+# Build the Debian package
+if ! dpkg-buildpackage -us -uc ; then
+	echo "[release] Failed to build Debian package"
+	exit 1
+fi
+
+# Tag the current commit in git
+echo "[release] Package built. Tagging current commit as v"$1
+if ! git tag -a v$1 && git push --tags ; then
+    echo "[release] Failed to tag commit. This is not a legitimate release!"
+    exit 1
+fi
+
+echo "[release] Commit tagged. Publishing to local repo..."
+
+# Add the new package to the local repo
+sudo reprepro -b /var/www/repos/apt/debian includedeb stretch ../clautod_$1_all.deb
+
+echo "[release] Package published. Cleaning up..."
+
+# Cleanup
+rm ../clautod_$1_all.deb
+rm ../clautod_$1_amd64.buildinfo
+rm ../clautod_$1_amd64.changes
+rm ../clautod_$1.dsc
+rm ../clautod_$1.tar.xz
+
+echo "[release] Cleaned up."
+echo "[release] clautod v"$1" is published."
