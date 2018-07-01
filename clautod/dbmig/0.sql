89d67478e35f78a46654021af5c4c0e78c7308bd
Jeremy Lerner <jeremy.quicklearner@gmail.com>
2018-07-01 11:19:19 -0700

    Added migration script 0

---
+++ clautod/dbmig/0.sql
@@ -0,0 +1,5 @@
+BEGIN TRANSACTION;
+
+PRAGMA user_version = 0;
+
+COMMIT;
--- clautod/dbmig/dbmig.sh
+++ clautod/dbmig/dbmig.sh
@@ -4,6 +4,17 @@
 # It should be run from the clautod postinst script on
 # installation of clautod.
 
-echo "[dbmig] HELLO I AM DBMIG"
+DB_FILE=/etc/clauto/clautod/clauto.db
 
-# If the database doesn't exist, create it
+# If the database doesn't exist, create it with USER_VERSION 0
+if [ ! -f $DB_FILE ] ; then
+    echo "[dbmig] Clauto database not found. Creating empty database."
+    touch $DB_FILE
+    sqlite3 $DB_FILE < 0.sql
+fi
+
+# Determine what version the existing database is at
+DB_VERSION_CURRENT=$(sqlite3 $DB_FILE 'PRAGMA user_version')
+
+# Determine what version the database should be
+DB_VERSION_GOAL=$(cat dbversion.txt)
