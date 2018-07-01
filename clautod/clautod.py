f0731d3c561ba7351dedcebb814a5cd6e9c88bd7
Jeremy Lerner <jeremy.quicklearner@gmail.com>
2018-06-30 18:00:15 -0700

    Fix

--- clautod/clautod.py
+++ clautod/clautod.py
@@ -1,5 +1,5 @@
 import time
 while(1):
     time.sleep(1)
-    with fopen(/home/jeremy/Projects/Clauto/HELLO.sh) as file:
+    with fopen("/home/jeremy/Projects/Clauto/HELLO.sh") as file:
         file.write("Hello I am clautod\n")
